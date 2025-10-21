8f520263f98cbc037d08c9b3140701c8af4ad60d
Rishabh426 <sharmarishabh9900@gmail.com>
2025-10-21 10:27:54 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Signup from "./pages/Signup"
 import Signin from "./pages/Signin"
 import { Blog } from "../components/Blog"
@@ -6,16 +6,16 @@ import Blogs from "./pages/Blogs"
 import { Publish } from "./pages/Publish"
 
 function App() {
-
-  return (  
+  return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/signup" replace />} />
+
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/blog/:id" element={<Blog />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/publish" element={<Publish />} />
-    
       </Routes>
     </BrowserRouter>
   )
