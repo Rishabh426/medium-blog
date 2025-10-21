a1dbb01cd1dd4a4f5c2379fcb9acbc5bd47a1347
Rishabh426 <sharmarishabh9900@gmail.com>
2025-10-21 10:13:46 +0530

    type issue

--- frontend/hooks/index.ts
+++ frontend/hooks/index.ts
@@ -1,57 +1,58 @@
 import { useEffect, useState } from "react"
-import axios from "axios"
+import axios, { type AxiosResponse } from "axios"
 import { BACKEND_URL } from "../config"
 
 export interface Blog {
-    content: string,
-    title: string,
-    id: number,
-    author: {
-        name: string,
-    }
+  content: string,
+  title: string,
+  id: number,
+  author: {
+    name: string,
+  }
 }
 
-export const useBlog = ({ id } : { id : string}) => {
-
-    const [loading, setLoading] = useState(true);
-    const [blog, setblog] = useState<Blog>();
-
-    useEffect(() => {
-        axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, {
-            headers: {
-                Authorization: localStorage.getItem("token"),
-            }
-        })
-        .then(response => {
-            setblog(response.data.blog);
-            setLoading(false);
-        }) 
-    }, [id]);
-
-    return {
-        loading,
-        blog,
-    }
+interface SingleBlogResponse {
+  blog: Blog
 }
+
+interface MultipleBlogsResponse {
+  blog: Blog[]
+}
+
+export const useBlog = ({ id }: { id: string }) => {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [blog, setBlog] = useState<Blog>()
+
+  useEffect(() => {
+    axios.get<SingleBlogResponse>(`${BACKEND_URL}/api/v1/blog/${id}`, {
+      headers: {
+        Authorization: localStorage.getItem("token") || ""
+      }
+    })
+    .then((response: AxiosResponse<SingleBlogResponse>) => {
+      setBlog(response.data.blog)
+      setLoading(false)
+    })
+  }, [id])
+
+  return { loading, blog }
+}
+
 export const useBlogs = () => {
-    
-    const [loading, setLoading] = useState(true);
-    const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [loading, setLoading] = useState<boolean>(true)
+  const [blogs, setBlogs] = useState<Blog[]>([])
 
-    useEffect(() => {
-        axios.get(`${BACKEND_URL}/api/v1/blog/bulk`, {
-            headers: {
-                Authorization: localStorage.getItem("token"),
-            }
-        })
-            .then(response => {
-                setBlogs(response.data.blog);
-                setLoading(false);
-            })
-    }, [])
+  useEffect(() => {
+    axios.get<MultipleBlogsResponse>(`${BACKEND_URL}/api/v1/blog/bulk`, {
+      headers: {
+        Authorization: localStorage.getItem("token") || ""
+      }
+    })
+    .then((response: AxiosResponse<MultipleBlogsResponse>) => {
+      setBlogs(response.data.blog)
+      setLoading(false)
+    })
+  }, [])
 
-    return {
-        loading,
-        blogs
-    }
-}   
+  return { loading, blogs }
+}
